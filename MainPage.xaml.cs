3a4bf4c8af8b95a764378f6d6a09a618d64efb22
Мирослав Недялков <19311050@students.bfu.bg>
2023-02-18 20:15:17 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using AngleSharp.Html.Parser;
+using Newtonsoft.Json;
+using Windows.Media.SpeechSynthesis;
+
+namespace JSON_Reader_1._0
+{
+    /// <summary>
+    /// Business Logic (BL): JSON Reader 1.0
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Constructor
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Button Get Joke Event Handler
+        private async void Button_Get_Click(object sender, RoutedEventArgs e)
+        {
+            // Download JSON
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync(new Uri("https://api.chucknorris.io/jokes/random"));
+
+            // Deserialize the JSON
+            var joke = JsonConvert.DeserializeObject<Root>(json);
+
+            // Parse the HTML
+            var html = new HtmlParser().ParseDocument(joke.value);
+            var text = html.Body.TextContent;
+
+            // Tell the Joke
+            Joke.Text = text;
+        }
+
+        // Button Tell Joke Event Handler
+        private async void Button_Tell_Click(object sender, RoutedEventArgs e)
+        {
+            if (Joke.Text != "")
+            {
+                // The media object for controlling and playing audio.
+                var mediaElement = new MediaElement();
+
+                // The object for controlling the speech synthesis engine (voice).
+                var synth = new SpeechSynthesizer();
+
+                // Generate the audio stream from plain text.
+                var stream = await synth.SynthesizeTextToStreamAsync(Joke.Text);
+
+                // Send the stream to the media object.
+                mediaElement.SetSource(stream, stream.ContentType);
+                mediaElement.Play();
+            }
+        }
+    }
+}
---
+++ Root.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace JSON_Reader_1._0
+{
+    public class Root
+    {
+        public List<object> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+}
